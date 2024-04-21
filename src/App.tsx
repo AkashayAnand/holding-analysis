3e2af871f494f72098842837d38fa5516bf7efeb
Akashay Anand <akshayanandvk@gmail.com>
2024-04-21 23:47:41 +0530

    basic layout to render data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
 import { useEffect, useState } from 'react'
 import HoldingService from './service/holdings.service'
 import { PayloadData } from './types/types';
+import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 
 function App() {
-  const holdingService = new HoldingService();
 
-  const [count, setCount] = useState<PayloadData[] | null>(null);
+  const holdingService = new HoldingService();
+  const [holdingData, setHoldingData] = useState<PayloadData[] | null>(null);
 
   useEffect(()=>{
     const fetchData = async () => {
       const data = await holdingService.getHoldings()
-      setCount(data);
+      setHoldingData(data);
     }
     fetchData();
   },[]);
 
   return (
-    <>
-      <h1>Hello</h1>
-      {count && count[0].asset_class}
+<>
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          Holdings
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Ticker</TableCell>
+                <TableCell align="right">Asset Class</TableCell>
+                <TableCell align="right">Average Price</TableCell>
+                <TableCell align="right">Market Price</TableCell>
+                <TableCell align="right">Market Value in CCY</TableCell>
+                <TableCell align="right">Latest Change (%)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {holdingData && holdingData.map((row, i) => (
+                <TableRow key={i}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.ticker}</TableCell>
+                  <TableCell align="right">{row.asset_class}</TableCell>
+                  <TableCell align="right">{row.avg_price?.toFixed(2)}</TableCell>
+                  <TableCell align="right">{row.market_price?.toFixed(2)}</TableCell>
+                  <TableCell align="right">{row.market_value_ccy.toFixed(2)}</TableCell>
+                  <TableCell align="right">{row.latest_chg_pct.toFixed(2)}%</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </>
   );
 };
